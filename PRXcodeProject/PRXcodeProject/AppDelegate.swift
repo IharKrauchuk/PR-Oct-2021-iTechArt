5b001a9f922666523a848a895fb1a4fa2fd17af7
Ihar Krauchuk <iKrauchuk@icloud.com>
2021-11-22 10:31:11 +0200

    Create Xcode project

---
+++ PRXcodeProject/PRXcodeProject/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  PRXcodeProject
+//
+//  Created by Ihar Krauchuk on 22.11.21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ PRXcodeProject/PRXcodeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PRXcodeProject
+//
+//  Created by Ihar Krauchuk on 22.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
